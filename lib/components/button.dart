1cfc4355bb83ae885a7907698f27d321efd0a9da
Gabriella Correia <gabriellacorreia@gec.inatel.br>
2020-07-09 18:09:48 -0300

    Operações implementadas

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-
-  static const DARK = Color.fromRGBO(82, 82, 82, 1);
+  static const DARK = Color.fromRGBO(102, 0, 51, 1);
   static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
-  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
+  static const OPERATION = Color.fromRGBO(67, 0, 33, 1);
 
   final String text;
   final bool big;
@@ -37,15 +36,16 @@ class Button extends StatelessWidget {
     return Expanded(
       flex: big ? 2 : 1,
       child: RaisedButton(
-        color: this.color,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-            fontWeight: FontWeight.w200,
-          )
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
         ),
+        color: this.color,
+        child: Text(text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.w200,
+            )),
         onPressed: () => cb(text),
       ),
     );
--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/Models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 
@@ -21,6 +22,11 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return MaterialApp(
       home: Column(
         children: <Widget>[
